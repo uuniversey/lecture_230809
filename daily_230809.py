240811af431f63cbda76f3c5480ec1c597e14ebf
이우주 <mm9326@naver.com>
2023-08-09 09:02:39 +0900

    first commit

---
+++ daily_230809.py
@@ -0,0 +1,42 @@
+
+
+# 1234. 비밀번호
+
+import sys
+sys.stdin = open('input_1234.txt', 'r')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 18224. 괄호검사
+
+# import sys
+# sys.stdin = open('input_18224.txt', 'r')
+
+# 18225. 반복문자 지우기
+
+# import sys
+# sys.stdin = open('input_18225.txt', 'r')
+
+# 18282. 연습문제2 괄호검사
+
+# import sys
+# sys.stdin = open('input_18282.txt', 'r')
+
+# 2005. 파스칼의 삼각형
+
+# import sys
+# sys.stdin = open('input_2005.txt', 'r')
